36505dd134a39a9fe45a823b5af0f1fb410829d0
flikkr <kazymir.rabier@gmail.com>
2021-04-13 22:23:11 +0800

    fixed map

--- src/pages/Map.js
+++ src/pages/Map.js
@@ -1,8 +1,12 @@
 import React, { useState, useEffect, useRef } from "react";
 import mapboxgl from "mapbox-gl/dist/mapbox-gl-csp";
+// eslint-disable-next-line import/no-webpack-loader-syntax
+import MapboxWorker from "worker-loader!mapbox-gl/dist/mapbox-gl-csp-worker";
+import "react-router-dom";
 
 import "./Map.scss";
 
+mapboxgl.workerClass = MapboxWorker;
 mapboxgl.accessToken =
   "pk.eyJ1IjoiZmxpa2tyIiwiYSI6ImNrbmVyYXIxcTJpemIzMXBoZ3JnMDZycDIifQ.5EneSaLoyECEbvPpp2XRxA";
 
@@ -11,7 +15,7 @@ export default function Map() {
   const [lng, setLng] = useState(-70.9);
   const [lat, setLat] = useState(42.35);
   const [zoom, setZoom] = useState(9);
-  
+
   useEffect(() => {
     const map = new mapboxgl.Map({
       container: mapContainer.current,
@@ -24,7 +28,10 @@ export default function Map() {
 
   return (
     <div>
-      <div className='map-container' ref={mapContainer} />
+      <div className='map-container' ref={mapContainer}></div>
+      <div className='map-overlay'>
+        <div className='map-navigator z-depth-5'></div>
+      </div>
     </div>
   );
 }
